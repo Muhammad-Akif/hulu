3fb74339bd7469ffb57f943a5939a4d10dbb6750
muhammad-akif <muhammadakif2917@gmail.com>
2022-03-07 15:33:33 +0500

    added HeaderItems and icons

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,12 @@
 import Image from 'next/image'
+import { BadgeCheckIcon, CollectionIcon, HomeIcon, LightningBoltIcon, SearchIcon, UserIcon} from "@heroicons/react"
+import HeaderItems from "./HeaderItems"
 export default function Header() {
     return (
         <header>
-            <h1>This is header</h1>
+            <div className="">
+                <HeaderItems title="HOME" Icon={HomeIcon} />
+            </div>
             <Image 
             className="object-contain" 
             src="https://www.nicepng.com/png/full/242-2424140_hulu-png-logo-hulu-logo-white-png.png"
---
+++ components/HeaderItems.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HeaderItems({ title, Icon}) {
+  return (
+    <div>HeaderItems</div>
+  )
+}
+
+export default HeaderItems

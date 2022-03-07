b95cfcb62f4ead1d1d9b481f65b2c8368396442b
muhammad-akif <muhammadakif2917@gmail.com>
2022-03-07 19:05:27 +0500

    updated navbar comp

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import requests from '..utils/requests'
 
 const Nav = () => {
   return (
     <nav>
-        
+      <div>
+        {Object.entries(requests).map(([key, { title, url }]) => (
+          <h2
+            key={key}
+            className=""
+          >
+            {title}
+          </h2>
+        ))}
+      </div>
     </nav>
-    )
+  )
 }
 
 export default Nav

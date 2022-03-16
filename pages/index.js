745573af0b73c2680b2893644b23a5473c566629
muhammad-akif <muhammadakif2917@gmail.com>
2022-03-16 22:07:33 +0500

    added server side props

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,9 @@ import Head from 'next/head'
 import Header from "../components/Header"
 import Nav from '../components/Nav'
 import Results from '../components/Results'
+import requests from '../utils/requests'
 export default function Home() {
+
   return (
     <div>
       <Head>
@@ -20,4 +22,17 @@ export default function Home() {
 
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+
+  const request = await fetch(`https://api.themoviedb.org/3${requests[genre]?.url || requests.fetchTrending?.url}`
+  ).then((res)=> res.json());
+
+  return {
+    props: {
+      results: request.results
+    }
+  }
 }

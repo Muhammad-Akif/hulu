5eebb24dea9bab4433874fdc8adc386ed571fb35
muhammad-akif <muhammadakif2917@gmail.com>
2022-03-17 00:58:14 +0500

    done with filp move library to swap b/e tabs

--- components/Results.jsx
+++ components/Results.jsx
@@ -4,11 +4,11 @@ import FilpMove from 'react-filp-move'
 
 const Results = ({ results }) => {
     return (
-        <div className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
+        <FilpMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
             {results.map(result => (
                 <Thumbnail key={result.id} result={result}/>
             ))}
-        </div>
+        </FilpMove>
     )
 }
 

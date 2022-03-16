367f05693e7e2cbbe33bb559f7d9c9199e15824a
muhammad-akif <muhammadakif2917@gmail.com>
2022-03-16 22:14:07 +0500

    data fetched successfully api

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,8 @@ import Header from "../components/Header"
 import Nav from '../components/Nav'
 import Results from '../components/Results'
 import requests from '../utils/requests'
-export default function Home() {
-
+export default function Home(props) {
+  console.log("results ==> ",props)
   return (
     <div>
       <Head>
@@ -27,12 +27,12 @@ export default function Home() {
 export async function getServerSideProps(context) {
   const genre = context.query.genre;
 
-  const request = await fetch(`https://api.themoviedb.org/3${requests[genre]?.url || requests.fetchTrending?.url}`
+  const request = await fetch(`https://api.themoviedb.org/3${requests[genre]?.url || requests.fetchTrending.url}`
   ).then((res)=> res.json());
 
   return {
     props: {
-      results: request.results
+      results: request.results,
     }
   }
 }

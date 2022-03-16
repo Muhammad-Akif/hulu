5149e5ef9b273a1737a30a6a5b5c0197f6e8fc04
muhammad-akif <muhammadakif2917@gmail.com>
2022-03-17 00:39:01 +0500

    added effects and jsx

--- components/Thumbnail.jsx
+++ components/Thumbnail.jsx
@@ -13,8 +13,13 @@ const Thumbnail = ({ result }) => {
             />
             <div className="p-2">
                 <p className="truncate max-w-md">{result.overview}</p>
-                <h2>{result.title || result.original_name}</h2>
-                <p></p>
+                <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out"
+                >{result.title || result.original_name}</h2>
+                <p className="flex flex-center opacity-0 group-hover:opacity-100">
+                    { result.media_type && `${result.media_type} .`}{" "}
+                    { result.release_date || result.first_air_date} . {" "}
+
+                </p>
             </div>
         </div>
     )
